eb83aecaaa9142dd7272eae8e6b724d334761f16
HyrumO <65050946+HyrumO@users.noreply.github.com>
2020-10-02 21:31:30 -0600

    write to file/no dups/fix readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # xkcd Scraper
 
 A scraper which will download all of the XKCD comics.
+
+All downloaded comics will go inside the ImageDownloads file
+
+To install all dependencys for this project use "pip install -r requirements.txt"
--- xkcd-scraper/scraper.py
+++ xkcd-scraper/scraper.py
@@ -1,6 +1,11 @@
 from bs4 import BeautifulSoup as bs
 import requests
 
+import os
+directoryPath = "Files"
+
+
+
 
 def main():
     url = 'https://xkcd.com/archive/'
@@ -38,19 +43,31 @@ def download_img(post_list: list):
     using the xkcd API
     """
 
-    for i in range(len(post_list)):
+    for i in range(4):
         # Adds the proper URL to get the json
         # information for each of the posts
         post_list[i] += 'info.0.json'
         post_json = requests.get(post_list[i]).json()
-        number, img_url = post_json['num'], post_json['img']
+        img_num, img_url = post_json['num'], post_json['img']
         title = post_json['title']
-
-        # Downloads the images and saves it as the post number
+        
+        #confimrs that it is actually downloading it
+        print(title+": "+str(img_num))
+        
+        # Downloads the images and saves it as the post number(img_num) to the directory "Files"
         img = requests.get(img_url)
-        with open(f'{number}.png', 'wb') as f:
-            f.write(img.content)
-            print(f'Downloaded {title}')
+        f  = open(f'ImageDownloads\\{img_num}.png','wb')
+        f.write(img.content)
+        f.close()
+
+        
+
+        
+
+
+
+     
+    
 
 
 if __name__ == '__main__':
